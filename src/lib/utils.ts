741b0f8788a68c3c4183bd4d256b3ebced3eb5ec
unknown <vitorgabrielsbo1460@gmail.com>
2026-01-21 20:45:53 -0300

    fix: remove fonts from ui lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,41 @@
 import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'bg-color': [
+        'bg-background',
+        'bg-foreground',
+        'bg-card',
+        'bg-popover',
+        'bg-primary',
+        'bg-secondary',
+        'bg-muted',
+        'bg-accent',
+        'bg-destructive'
+      ],
+      'text-color': [
+        'text-foreground',
+        'text-background',
+        'text-primary',
+        'text-secondary',
+        'text-muted',
+        'text-accent',
+        'text-destructive'
+      ],
+      'border-color': [
+        'border-border',
+        'border-input',
+        'border-primary',
+        'border-secondary',
+        'border-accent',
+        'border-destructive'
+      ]
+    }
+  }
+})
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return customTwMerge(clsx(inputs))
 }
