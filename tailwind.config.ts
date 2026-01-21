8db1c3e96f102ac4050902a7b93238f9e72e2700
unknown <vitorgabrielsbo1460@gmail.com>
2026-01-21 18:48:19 -0300

    refactor: remove vite stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config}  */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{ts,tsx}', './stories/**/*.{ts,tsx}'],
   theme: {
     extend: {
       screens: {
