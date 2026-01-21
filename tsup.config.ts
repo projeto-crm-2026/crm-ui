031774b0fed0133c95e37c00d19db9c456192914
unknown <vitorgabrielsbo1460@gmail.com>
2026-01-21 15:04:46 -0300

    chore: add postcss-import

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,35 +1,41 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import postcss from 'postcss'
+import postcssImport from 'postcss-import'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-
   format: ['esm', 'cjs'],
   dts: true,
   tsconfig: './tsconfig.app.json',
-
   splitting: true,
   sourcemap: true,
   clean: true,
   external: ['react', 'react-dom', 'react/jsx-runtime'],
-  treeshake: true,
-  minify: false,
+
   esbuildOptions(options) {
     options.banner = {
       js: '"use client";'
     }
   },
   async onSuccess() {
-    const fs = await import('fs')
-    const path = await import('path')
+    const srcCssPath = path.join(process.cwd(), 'src/assets/global.css')
+    const distCssPath = path.join(process.cwd(), 'dist/index.css')
 
-    const srcCss = path.join(process.cwd(), 'src/assets/global.css')
-    const distCss = path.join(process.cwd(), 'dist/index.css')
-
-    if (fs.existsSync(srcCss)) {
-      if (!fs.existsSync(path.dirname(distCss))) {
-        fs.mkdirSync(path.dirname(distCss), { recursive: true })
+    if (fs.existsSync(srcCssPath)) {
+      if (!fs.existsSync(path.dirname(distCssPath))) {
+        fs.mkdirSync(path.dirname(distCssPath), { recursive: true })
       }
-      fs.copyFileSync(srcCss, distCss)
+
+      const cssContent = fs.readFileSync(srcCssPath, 'utf8')
+
+      const result = await postcss([postcssImport()]).process(cssContent, {
+        from: srcCssPath,
+        to: distCssPath
+      })
+
+      fs.writeFileSync(distCssPath, result.css)
     }
   }
 })
