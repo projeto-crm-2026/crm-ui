3555366cc1b610fdb9dc366885a993b394b2dcbb
unknown <vitorgabrielsbo1460@gmail.com>
2026-01-21 22:56:32 -0300

    feat: improve tailwnind preset file

--- src/tailwind/preset.ts
+++ src/tailwind/preset.ts
@@ -1,8 +1,71 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 const preset: Config = {
   darkMode: ['class', 'dark'],
-  plugins: [require('tailwindcss-animate')]
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        primary: 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        muted: 'var(--muted)',
+        'muted-foreground': 'var(--muted-foreground)',
+        accent: 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        destructive: 'var(--destructive)',
+        'destructive-foreground': 'var(--destructive-foreground)',
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        sidebar: 'var(--sidebar)',
+        'sidebar-foreground': 'var(--sidebar-foreground)',
+        'sidebar-primary': 'var(--sidebar-primary)',
+        'sidebar-primary-foreground': 'var(--sidebar-primary-foreground)',
+        'sidebar-accent': 'var(--sidebar-accent)',
+        'sidebar-accent-foreground': 'var(--sidebar-accent-foreground)',
+        'sidebar-border': 'var(--sidebar-border)',
+        'sidebar-ring': 'var(--sidebar-ring)',
+        chart1: 'var(--chart-1)',
+        chart2: 'var(--chart-2)',
+        chart3: 'var(--chart-3)',
+        chart4: 'var(--chart-4)',
+        chart5: 'var(--chart-5)'
+      },
+      borderRadius: {
+        DEFAULT: 'var(--radius)',
+        sm: 'var(--radius-sm)',
+        md: 'var(--radius-md)',
+        lg: 'var(--radius-lg)',
+        xl: 'var(--radius-xl)',
+        '2xl': 'var(--radius-2xl)'
+      },
+      boxShadow: {
+        '2xs': 'var(--shadow-2xs)',
+        xs: 'var(--shadow-xs)',
+        sm: 'var(--shadow-sm)',
+        DEFAULT: 'var(--shadow)',
+        md: 'var(--shadow-md)',
+        lg: 'var(--shadow-lg)',
+        xl: 'var(--shadow-xl)',
+        '2xl': 'var(--shadow-2xl)'
+      }
+    }
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+    plugin(function ({ addBase }) {
+      addBase({
+        '*': { 'border-color': 'var(--border)', outline: 'var(--ring)/50' },
+        body: { background: 'var(--background)', color: 'var(--foreground)' }
+      })
+    })
+  ]
 }
 
 export default preset
